ed2388f1d96a78942006ff7d1b97b738688d75c3
Jhon <jhon.charryserrato@gmail.com>
2022-01-16 20:41:36 -0500

    Preparing to production

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,18 +1,18 @@
-'use strict'
+"use strict";
 
-var jwt = require('jwt-simple');
-var moment = require('moment');
-var secret = 'pragol2021Monithor';
+var jwt = require("jwt-simple");
+var moment = require("moment");
+var secret = "pragol2021Monithor";
 
-exports.createToken = function(user){
-    var payload = {
-        sub: user._id,
-        nombres: user.nombres,
-        apellidos: user.apellidos,
-        email: user.email,
-        iat: moment().unix(),
-        exp: moment().add(1,'day').unix()
-    }
+exports.createToken = function (user) {
+  var payload = {
+    sub: user._id,
+    nombres: user.nombres,
+    apellidos: user.apellidos,
+    email: user.email,
+    iat: moment().unix(),
+    exp: moment().add(1, "day").unix(),
+  };
 
-    return jwt.encode(payload,secret);
-}
+  return jwt.encode(payload, process.env.TOKEN_SECRET);
+};
